7070759fb1accb513c7c4372f5c0a42d258139ce
gythialy <gythialy.koo+github@gmail.com>
2021-05-29 18:12:02 +0800

    feat: add example project

---
+++ example/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3" // Import go-sqlite3 library
+)
+
+// https://www.codeproject.com/Articles/5261771/Golang-SQLite-Simple-Example
+func main() {
+	os.Remove("sqlite-database.db") // I delete the file to avoid duplicated records.
+	// SQLite is a file based database.
+
+	log.Println("Creating sqlite-database.db...")
+	file, err := os.Create("sqlite-database.db") // Create SQLite file
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	file.Close()
+	log.Println("sqlite-database.db created")
+
+	sqliteDatabase, _ := sql.Open("sqlite3", "./sqlite-database.db") // Open the created SQLite File
+	defer sqliteDatabase.Close()                                     // Defer Closing the database
+	createTable(sqliteDatabase)                                      // Create Database Tables
+
+	// INSERT RECORDS
+	insertStudent(sqliteDatabase, "0001", "Liana Kim", "Bachelor")
+	insertStudent(sqliteDatabase, "0002", "Glen Rangel", "Bachelor")
+	insertStudent(sqliteDatabase, "0003", "Martin Martins", "Master")
+	insertStudent(sqliteDatabase, "0004", "Alayna Armitage", "PHD")
+	insertStudent(sqliteDatabase, "0005", "Marni Benson", "Bachelor")
+	insertStudent(sqliteDatabase, "0006", "Derrick Griffiths", "Master")
+	insertStudent(sqliteDatabase, "0007", "Leigh Daly", "Bachelor")
+	insertStudent(sqliteDatabase, "0008", "Marni Benson", "PHD")
+	insertStudent(sqliteDatabase, "0009", "Klay Correa", "Bachelor")
+
+	// DISPLAY INSERTED RECORDS
+	displayStudents(sqliteDatabase)
+}
+
+func createTable(db *sql.DB) {
+	createStudentTableSQL := `CREATE TABLE student (
+		"idStudent" integer NOT NULL PRIMARY KEY AUTOINCREMENT,		
+		"code" TEXT,
+		"name" TEXT,
+		"program" TEXT		
+	  );` // SQL Statement for Create Table
+
+	log.Println("Create student table...")
+	statement, err := db.Prepare(createStudentTableSQL) // Prepare SQL Statement
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	statement.Exec() // Execute SQL Statements
+	log.Println("student table created")
+}
+
+// We are passing db reference connection from main to our method with other parameters
+func insertStudent(db *sql.DB, code string, name string, program string) {
+	log.Println("Inserting student record ...")
+	insertStudentSQL := `INSERT INTO student(code, name, program) VALUES (?, ?, ?)`
+	statement, err := db.Prepare(insertStudentSQL) // Prepare statement.
+	// This is good to avoid SQL injections
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	_, err = statement.Exec(code, name, program)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+func displayStudents(db *sql.DB) {
+	row, err := db.Query("SELECT * FROM student ORDER BY name")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer row.Close()
+	for row.Next() { // Iterate and fetch the records from result cursor
+		var id int
+		var code string
+		var name string
+		var program string
+		row.Scan(&id, &code, &name, &program)
+		log.Println("Student: ", code, " ", name, " ", program)
+	}
+}
