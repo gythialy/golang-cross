ac1bea2a8bd4974c416fcf3c88fea5fb320f5068
Esteban Ricardo Mirizio <ermirizio@gmail.com>
2022-03-31 15:38:26 -0300

    Harden package cloud push

--- pc.sh
+++ pc.sh
@@ -5,9 +5,12 @@
 # this script bridges both by choosing the appropriate list of distro versions from $DEBVERS and $RPMVERS
 # $REPO, $DEBVERS and $RPMVERS are expected to be set by goreleaser
 
+REQUIRED_VARS="PACKAGECLOUD_TOKEN REPO"
+
 usage() {
     echo <<EOF
 Usage: $0 pkg_file
+Required envs: ${REQUIRED_VARS[@]}
 EOF
 }
 
@@ -16,8 +19,19 @@ if [ -z $1 ]; then
     exit 1
 fi
 
-pkg=$1
+for variable in ${REQUIRED_VARS}; do
+	if [[ -z "${!variable}" ]]; then
+		echo "Variable: $variable - is missing"
+		exit 1
+	fi
+done
 
+if [[ -z "$DEBVERS" ]] && [[ -z "$RPMVERS" ]]; then
+	echo "Please define at least DEBVERS or RPMVERS vars";
+	exit 1
+fi
+
+pkg=$1
 case $pkg in
     *PAYG*)
 	echo Not uploading PAYG version $pkg
